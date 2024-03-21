8a3dada74bc65a5d8c0d6b6cd8ee5dfb1778c891
Ernesto Melian <you@example.com>
2024-03-21 14:36:32 -0300

    adding loading data

--- src/App.jsx
+++ src/App.jsx
@@ -87,35 +87,52 @@ export default function App() {
             {error && <p className="font-monospace text-danger-emphasis">{error}</p>}
          </form>
 
-         {/* TODO LIST */}
-         {todos.filter(todo => !todo.completed).length !== 0 && (<div className='todo-list-container row gy-2 mx-0'>
-            <div className='d-flex align-items-center justify-content-between'>
-               <ListTitle>TO DO [{todos.filter(todo => !todo.completed).length}]</ListTitle>
-               <button type="button" className="btn btn-outline-success border-0 opacity-75" title='Mark all as completed' onClick={handleAllCompleted}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-all" viewBox="0 0 16 16">
-                     <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z"/>
-                  </svg>
-               </button>
+         {todos.length === 0 && (
+            <div className="d-flex column-gap-2 align-items-center justify-content-center">
+               <div className="spinner-border text-warning spinner-border-sm" role="status">
+                  <span className="visually-hidden">Loading...</span>
+               </div>
+               <span className="text-warning-emphasis">Retrieven data</span>
             </div>
-            {todos.map((todo) => (
-               !todo.completed ? <TodoItem key={todo.id} todo={todo} handleComplete={handleComplete} handleDelete={handleDelete}/> : null
-            ))}
-         </div>)}
+            )}
 
-         {/* COMPLETED LIST */}
-         {todos.filter(todo => todo.completed).length !== 0 && (<div className='todo-completed-container row gy-2 mx-0 mt-3'>
-            <div className='d-flex align-items-center justify-content-between'>
-               <ListTitle>COMPLETED [{todos.filter(todo => todo.completed).length}]</ListTitle>
-               <button type="button" className="btn btn-outline-danger border-0 opacity-50" title='Delete all completed' onClick={handleAllDelete}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
-                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"></path>
-                  </svg>
-               </button>
+         { /* TODO LIST */
+            todos.filter(
+            todo => !todo.completed).length === 0 || (
+               <div className='todo-list-container row gy-2 mx-0'> {/* CONDITIONAL WITH || */}
+                  <div className='d-flex align-items-center justify-content-between'>
+                     <ListTitle>TO DO [{todos.filter(todo => !todo.completed).length}]</ListTitle>
+                     <button type="button" className="btn btn-outline-success border-0 opacity-75" title='Mark all as completed' onClick={handleAllCompleted}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-all" viewBox="0 0 16 16">
+                           <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z"/>
+                        </svg>
+                     </button>
+                  </div>
+                  {todos.map((todo) => (
+                     !todo.completed && <TodoItem key={todo.id} todo={todo} handleComplete={handleComplete} handleDelete={handleDelete}/>
+                  ))}
+               </div>
+            )
+         }
+
+         {/* COMPLETED LIST */
+         todos.filter(todo => todo.completed).length !== 0 && (
+            <div className='todo-completed-container row gy-2 mx-0 mt-3'> {/* SAME CONDITIONAL BUT WITH && */}
+               <div className='d-flex align-items-center justify-content-between'>
+                  <ListTitle>COMPLETED [{todos.filter(todo => todo.completed).length}]</ListTitle>
+                  <button type="button" className="btn btn-outline-danger border-0 opacity-50" title='Delete all completed' onClick={handleAllDelete}>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"></path>
+                     </svg>
+                  </button>
+               </div>
+               {todos.map((todo) => (
+                  todo.completed && <TodoItem key={todo.id} todo={todo} handleComplete={handleComplete} handleDelete={handleDelete}/>
+               ))}
             </div>
-            {todos.map((todo) => (
-               todo.completed ? <TodoItem key={todo.id} todo={todo} handleComplete={handleComplete} handleDelete={handleDelete}/> : null
-            ))}
-         </div>)}
+            )
+         }
+         
       </div>
   )
 
